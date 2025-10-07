8f9ddb8ff66a8220d559a4d12cd706ed94ca018e
alexwaya0 <Alexander Nguku>
2025-10-07 13:06:29 +0300

    Fix template tags tag loading

--- lms/templatetags/lms_tags.py
+++ lms/templatetags/lms_tags.py
@@ -4,4 +4,8 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
+    """
+    Custom filter to access dictionary items safely in templates.
+    Usage: {{ my_dict|get_item:key }}
+    """
     return dictionary.get(key)
